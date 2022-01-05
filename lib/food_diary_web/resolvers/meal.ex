a816458ecf57b1923c6939774f1cda0ce89a934a
LuizFerK <luizfernandoklein@live.com>
2022-01-05 18:11:55 -0300

    feat: Meal subscription

--- lib/food_diary_web/resolvers/meal.ex
+++ lib/food_diary_web/resolvers/meal.ex
@@ -1,7 +1,12 @@
 defmodule FoodDiaryWeb.Resolvers.Meal do
+  alias Absinthe.Subscription
   alias FoodDiary.Meals.Create
+  alias FoodDiaryWeb.Endpoint
 
   def create(%{input: params}, _context) do
-    Create.call(params)
+    {:ok, meal} = Create.call(params)
+    Subscription.publish(Endpoint, meal, new_meal: "new_meal_topic")
+
+    {:ok, meal}
   end
 end
